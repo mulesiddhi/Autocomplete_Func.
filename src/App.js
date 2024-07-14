c1b3f5f57b0974a24b2b8703e523265a86d3868a
smule <smule@ncsu.edu>
2024-07-13 21:05:17 -0400

    debounce hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+
+import  useDebounce  from './custom-hooks/useDebounce';
+
 function App() {
+  const [data, setData] = useState(null);
+  const loadData = async (event) => {
+    const val = event.target.value;
+    if (val===''){
+      setData(null);
+      return;
+    }
+    try {
+      const response = await fetch(`https://restcountries.com/v3.1/name/${val}`);
+      if (response.ok) {
+        const res = await response.json();
+        setData(res);
+      } else {
+        setData([]);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setData([]);
+    }
+  };
+  const loadDataDebounced = useDebounce(loadData, 400)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      autocomplete
+      <p>
+      <input type='text' onChange={loadDataDebounced} />
+      <div>
+      {data && data.length !== 0 && (
+          <div>
+            {data.map((country) => (
+              <div key={country.cca2}>{country.name.common}</div>
+            ))}
+          </div>
+        )}
+      </div>
+        
+      </p>
       </header>
+      
     </div>
   );
 }
